2bcdd04235bc671c2181cda5529a71c467cf3bfe
danilone123 <danielvelascozarate@gmail.com>
2016-01-11 10:00:57 -0300

    injector

---
+++ Injector/Link.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIRenderers;
+using CommonUtils;
+
+namespace Injector
+{
+    public class Link
+    {
+        public Link( Usuarios us )
+        {
+            mainForm main = new mainForm(  );
+            main.UsActual = us;
+            main.Show( );
+        }
+    }
+}
